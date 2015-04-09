7d0694739b010edc248829da2fcf8d674d44a20d
gaxus <lordyoum@hotmail.fr>
2015-04-09 12:18:03 +0200

    test

---
+++ jenkins-testlink-selenium/src/test/java/com/example/tests/Selenium_junit4_remote_controlTest.java
@@ -0,0 +1,37 @@
+package com.example.tests;
+
+import com.thoughtworks.selenium.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.regex.Pattern;
+
+public class Selenium_junit4_remote_controlTest {
+	private Selenium selenium;
+
+	@Before
+	public void setUp() throws Exception {
+		selenium = new DefaultSelenium("localhost", 4444, "*chrome", "http://www.payline.com/");
+		selenium.start();
+	}
+
+	@Test
+	public void testSelenium_junit4_remote_control() throws Exception {
+		selenium.open("/index.php/fr/");
+		selenium.click("css=a.subMenuBtn > span");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("css=li.item-173 > a > span");
+		selenium.waitForPageToLoad("30000");
+		assertTrue(selenium.isTextPresent(""));
+		selenium.click("css=li.item-176 > a.subMenuBtn > span");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("css=li.item-181.first > a.subMenuBtn > span");
+		selenium.waitForPageToLoad("30000");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		selenium.stop();
+	}
+}
