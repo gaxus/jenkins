5049a8511ee96f4c716786cf7f4180ccd59ae3db
gaxus <lordyoum@hotmail.fr>
2015-04-10 14:23:48 +0200

    Junit based project

---
+++ jenkins-testlink-junit-soapui/src/test/java/com/payline/test/SoapuiJunitTest.java
@@ -0,0 +1,25 @@
+package com.payline.test;
+
+import com.eviware.soapui.impl.wsdl.WsdlProject;
+import com.eviware.soapui.model.support.PropertiesMap;
+import com.eviware.soapui.model.testsuite.TestCase;
+import com.eviware.soapui.model.testsuite.TestRunner;
+import com.eviware.soapui.model.testsuite.TestRunner.Status;
+import com.eviware.soapui.model.testsuite.TestSuite;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SoapuiJunitTest {
+	
+	@Test
+	public void testRunner() throws Exception 
+	{
+	  WsdlProject project = new WsdlProject("src/test/resources/Jenkins-soapui-project.xml"); 
+	  TestSuite testSuite = project.getTestSuiteByName( "NR FDJ" ); 
+	  TestCase testCase = testSuite.getTestCaseByName( "T01" );
+	  
+	  TestRunner runner = testCase.run( new PropertiesMap(), false );
+	  Assert.assertEquals(Status.FINISHED, runner.getStatus());
+	}
+}
