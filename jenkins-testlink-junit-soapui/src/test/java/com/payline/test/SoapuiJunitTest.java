5a74051328a224c626b56d6ef8e85b34e409c1e0
gaxus <lordyoum@hotmail.fr>
2015-04-13 09:08:53 +0200

    trying to resolve ssl issue

--- jenkins-testlink-junit-soapui/src/test/java/com/payline/test/SoapuiJunitTest.java
+++ jenkins-testlink-junit-soapui/src/test/java/com/payline/test/SoapuiJunitTest.java
@@ -11,15 +11,17 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SoapuiJunitTest {
-	
+
 	@Test
 	public void testRunner() throws Exception 
 	{
-	  WsdlProject project = new WsdlProject("src/test/resources/Jenkins-soapui-project.xml"); 
-	  TestSuite testSuite = project.getTestSuiteByName( "NR FDJ" ); 
-	  TestCase testCase = testSuite.getTestCaseByName( "T01" );
-	  
-	  TestRunner runner = testCase.run( new PropertiesMap(), false );
-	  Assert.assertEquals(Status.FINISHED, runner.getStatus());
+		System.setProperty("jsse.enableSNIExtension", "false");
+		System.setProperty("com.sun.net.ssl.checkRevocation", "false");
+		WsdlProject project = new WsdlProject("src/test/resources/Jenkins-soapui-project.xml"); 
+		TestSuite testSuite = project.getTestSuiteByName( "NR FDJ" ); 
+		TestCase testCase = testSuite.getTestCaseByName( "T01" );
+
+		TestRunner runner = testCase.run( new PropertiesMap(), false );
+		Assert.assertEquals(Status.FINISHED, runner.getStatus());
 	}
 }
