552b59a9ef7fd3865b343cbf0402f6d52036de2a
gaxus <lordyoum@hotmail.fr>
2015-04-24 16:46:45 +0200

    soapui calls selenium dowebpayment

--- jenkins-testlink-junit-soapui/src/test/java/com/payline/test/SoapuiJunitTest.java
+++ jenkins-testlink-junit-soapui/src/test/java/com/payline/test/SoapuiJunitTest.java
@@ -1,5 +1,8 @@
 package com.payline.test;
 
+import java.net.ProxySelector;
+import java.util.List;
+
 import com.eviware.soapui.impl.wsdl.WsdlProject;
 import com.eviware.soapui.model.support.PropertiesMap;
 import com.eviware.soapui.model.testsuite.TestCase;
@@ -15,13 +18,17 @@ public class SoapuiJunitTest {
 	@Test
 	public void testRunner() throws Exception 
 	{
-		System.setProperty("jsse.enableSNIExtension", "false");
-		System.setProperty("com.sun.net.ssl.checkRevocation", "false");
-		WsdlProject project = new WsdlProject("src/test/resources/Jenkins-soapui-project.xml"); 
-		TestSuite testSuite = project.getTestSuiteByName( "NR FDJ" ); 
-		TestCase testCase = testSuite.getTestCaseByName( "T01" );
-
-		TestRunner runner = testCase.run( new PropertiesMap(), false );
-		Assert.assertEquals(Status.FINISHED, runner.getStatus());
+		//WsdlProject project = new WsdlProject("src/test/resources/Jenkins-soapui-project.xml");
+		ProxySelector proxy = ProxySelector.getDefault();
+		WsdlProject project = new WsdlProject("src/test/resources/project-t02-groovy.xml");
+		ProxySelector.setDefault(proxy);
+		List<TestSuite> testSuites = project.getTestSuiteList();
+		for(TestSuite ts : testSuites){
+			List<TestCase> testCases = ts.getTestCaseList();
+			for(TestCase tc : testCases){
+				TestRunner runner = tc.run(new PropertiesMap(), false);
+				Assert.assertEquals(Status.FINISHED, runner.getStatus());
+			}
+		}
 	}
 }
