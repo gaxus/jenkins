0a877b3516ccbf3d11f4aca5797a9439afe7ace1
gaxus <lordyoum@hotmail.fr>
2015-04-28 09:16:10 +0200

    Nettoyage de printemps

--- jenkins-testlink-junit-soapui/src/test/java/com/payline/test/SoapuiJunitTest.java
+++ jenkins-testlink-junit-soapui/src/test/java/com/payline/test/SoapuiJunitTest.java
@@ -13,15 +13,25 @@ import com.eviware.soapui.model.testsuite.TestSuite;
 import org.junit.Assert;
 import org.junit.Test;
 
+/**
+ * 
+ * @author Charlélie Bouvier - Monext 04/2015
+ * Squelette pour test SoapUI
+ *
+ */
 public class SoapuiJunitTest {
 
+	/**
+	 * 
+	 * @throws Exception
+	 * Récupère un projet xml SoapUI, crée une instance WsdlProject et execute chacun des tests.
+	 */
 	@Test
 	public void testRunner() throws Exception 
 	{
-		//WsdlProject project = new WsdlProject("src/test/resources/Jenkins-soapui-project.xml");
-		ProxySelector proxy = ProxySelector.getDefault();
+		ProxySelector proxy = ProxySelector.getDefault(); // On récupère le proxy courant.
 		WsdlProject project = new WsdlProject("file:///"+System.getProperty("user.dir")+"/src/test/resources/project-t02-groovy.xml");
-		ProxySelector.setDefault(proxy);
+		ProxySelector.setDefault(proxy); //On set le proxy précédemment enregistré car l'instanciation d'un WsdlProject efface le proxy, et cause des problèmes avec FirefoxWebdriver. 
 		List<TestSuite> testSuites = project.getTestSuiteList();
 		for(TestSuite ts : testSuites){
 			List<TestCase> testCases = ts.getTestCaseList();
